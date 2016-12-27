5f136242d56b58a7110e6b71680cddbebdcef642
高原 <tom@gaoyuandeMacBook-Pro.local>
2016-12-27 21:44:27 +0800

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by 高原 on 2016/12/27.
+//  Copyright © 2016年 高原. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
